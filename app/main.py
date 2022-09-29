c95ddd5d497354861dbf8c653515feec0fd520e5
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-09-29 08:21:24 +0200

    Add ability to read cipher key from Secret Manager (#1)
    * Read cipher key from Secret Manager when using the 'sm://' pattern from Berglas environment variable reference syntax.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import logging.config
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from pythonjsonlogger import jsonlogger
 from prometheus_fastapi_instrumentator import Instrumentator
 from app import __version__
 from app.types import EncryptionRequest, EncryptionResponse
 from aes_pkcs5.algorithms.aes_ecb_pkcs5_padding import AESECBPKCS5Padding
+from google.cloud.secretmanager import SecretManagerServiceClient
 
 app = FastAPI()
 
@@ -23,6 +24,8 @@ logger.handlers[0].setFormatter(formatter)
 
 # Metrics
 Instrumentator(excluded_handlers=["/health/.*", "/metrics"]).instrument(app).expose(app)
+
+SECRET_MANAGER_PREFIX = "sm://"
 
 
 @app.get("/health/liveness", status_code=200)
@@ -43,20 +46,33 @@ def health_readiness():
     }
 
 
+def get_sm_client() -> SecretManagerServiceClient:
+    return SecretManagerServiceClient()
+
+
 @app.post("/encrypt", status_code=200, response_model=EncryptionResponse)
-def encrypt(request: EncryptionRequest):
+def encrypt(request: EncryptionRequest, sm_client: SecretManagerServiceClient = Depends(get_sm_client)):
     """Encrypts a message with a given key. Uses AES with CBC/ECB mode and padding scheme PKCS5."""
     if 'CIPHER_KEY' not in os.environ:
         error_msg = 'Missing environment variable: CIPHER_KEY'
         logger.exception("Error occurred: %s", error_msg)
         raise HTTPException(status_code=500, detail=error_msg)
 
-    key = os.environ['CIPHER_KEY']
-    if len(key) != 16:
+    cipher_key = os.environ['CIPHER_KEY']
+    # The cipher key can either be stored directly in the environment variable, or via Secret Manager
+    if cipher_key.startswith(SECRET_MANAGER_PREFIX):
+        project_id, secret_id, version = get_project_and_name(cipher_key)
+        # Build the resource name of the secret version.
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/{version}"
+        # Access the secret version.
+        response = sm_client.access_secret_version(request={"name": name})
+        cipher_key = response.payload.data.decode("UTF-8")
+
+    if len(cipher_key) != 16:
         error_msg = 'CIPHER_KEY must be of length 16'
         logger.exception("Error occurred: %s", error_msg)
         raise HTTPException(status_code=500, detail=error_msg)
-    cipher = AESECBPKCS5Padding(key, "b64")
+    cipher = AESECBPKCS5Padding(cipher_key, "b64")
     return {
         "message": cipher.encrypt(request.message)
     }
@@ -66,3 +82,34 @@ def encrypt(request: EncryptionRequest):
 def app_startup():
     """Does some initial startup stuff"""
     logger.info(f"Starting Statbank Authenticator version {__version__} ...")
+
+
+def get_project_and_name(env_var_value: str) -> (str, str, str):
+    """
+    Split the env_var_value into project ID, name and version, where env_var_value should match the
+    Secret Manager pattern from Berglas environment variable reference syntax (see
+    https://github.com/GoogleCloudPlatform/berglas/blob/main/doc/reference-syntax.md).
+    :param env_var_value: should respect this pattern sm://[PROJECT]/[NAME]#[VERSION], <VERSION> is optional
+    :return: the project ID, the name and the version
+    :exception: When PROJECT and/or NAME is missing (pattern not respected)
+    """
+
+    without_prefix = env_var_value[len(SECRET_MANAGER_PREFIX):]
+    if without_prefix == "":
+        log_msg = f"No project ID and name defined in {env_var_value}"
+        logging.error(log_msg)
+        raise EnvironmentError(log_msg)
+
+    # Split [PROJECT]/[NAME]#[VERSION]
+    splitted = without_prefix.split("/", 1)
+
+    if splitted[1] == "":
+        log_msg = f"No name defined in {env_var_value}"
+        logging.error(log_msg)
+        raise EnvironmentError(log_msg)
+
+    # Split optional VERSION part
+    if "#" in splitted[1]:
+        return splitted[0], *splitted[1].split("#", 1)
+    else:
+        return splitted[0], splitted[1], 'latest'
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,9 +4,16 @@ from fastapi.testclient import TestClient
 
 from pathlib import Path
 from app import __version__
-from app.main import app
+from app.main import app, get_project_and_name, get_sm_client
+from google.cloud.secretmanager import SecretManagerServiceClient, AccessSecretVersionResponse, SecretPayload
+from google.auth import credentials
+
+CIPHER_KEY_16_DIGIT = '@NcRfUjXn2r5u8x/'
 
 client = TestClient(app)
+# Local instance of Secret Manager Service that can be used to fake reponses
+sm_client = SecretManagerServiceClient(credentials=credentials.AnonymousCredentials)
+app.dependency_overrides[get_sm_client] = lambda: sm_client
 
 
 def test_versions_are_in_sync():
@@ -21,8 +28,25 @@ def test_versions_are_in_sync():
     assert package_init_version == pyproject_version
 
 
+def test_encrypt_without_env_variable():
+    response = client.post("/encrypt", json={
+        "message": "mysecretmessage"
+    })
+    assert response.status_code == 500
+    assert response.json() == {'detail': 'Missing environment variable: CIPHER_KEY'}
+
+
+def test_encrypt_with_invalid_cipher():
+    os.environ['CIPHER_KEY'] = 'invalid_cipher'
+    response = client.post("/encrypt", json={
+        "message": "mysecretmessage"
+    })
+    assert response.status_code == 500
+    assert response.json() == {'detail': 'CIPHER_KEY must be of length 16'}
+
+
 def test_encrypt():
-    os.environ['CIPHER_KEY'] = '@NcRfUjXn2r5u8x/'
+    os.environ['CIPHER_KEY'] = CIPHER_KEY_16_DIGIT
     response = client.post("/encrypt", json={
         "message": "mysecretmessage"
     })
@@ -31,3 +55,33 @@ def test_encrypt():
     assert response.json() == {
         "message": 'dQB+Eaf751En6/j4TQtrcg=='
     }
+
+
+def test_encrypt_with_sm_key():
+    # Fake the response from Secret Manager
+    sm_client.access_secret_version = lambda request: AccessSecretVersionResponse(
+        payload=SecretPayload(data=CIPHER_KEY_16_DIGIT.encode("UTF-8"))
+    )
+    os.environ['CIPHER_KEY'] = 'sm://my-project/my-key'
+    response = client.post("/encrypt", json={
+        "message": "mysecretmessage"
+    })
+    assert response.status_code == 200
+    # The same cipher will always create the same result
+    assert response.json() == {
+        "message": 'dQB+Eaf751En6/j4TQtrcg=='
+    }
+
+
+def test_parse_with_default_version():
+    project_id, name, version = get_project_and_name('sm://my-project/my-secret')
+    assert project_id == 'my-project'
+    assert name == 'my-secret'
+    assert version == 'latest'
+
+
+def test_parse_with_version():
+    project_id, name, version = get_project_and_name('sm://my-project/my-secret#1')
+    assert project_id == 'my-project'
+    assert name == 'my-secret'
+    assert version == '1'
