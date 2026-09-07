77127fc6e989972e7c4f1d61326817ecfc53899d
Nicholas <3789764+skykanin@users.noreply.github.com>
2026-09-07 10:20:34 +0200

    chore/new release (#192)
    * nix: update flake
    
    * Bump version: 0.5.7 → 0.5.8

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
   };
 
   outputs = inputs @ {flake-parts, ...}:
